48741735327024cb21378a31f9d3286823dea781
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-07-14 19:10:18 +0530

    Add AMM error handling, deposit, initialize, and withdraw instructions

---
+++ amm/programs/amm/src/lib.rs
@@ -0,0 +1,36 @@
+#![allow(unexpected_cfgs)]
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("9SDA8RdCJYAb841afZC42crsTj4cN5ateUz2KSXAbsgy");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn init(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)?;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, min_x, min_y)?;
+        Ok(())
+    }
+}
