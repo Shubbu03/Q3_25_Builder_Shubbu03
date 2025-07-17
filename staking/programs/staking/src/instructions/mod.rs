be8f0ca5fef82d67c7b59278c47e8651f00dbeae
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-07-17 21:23:43 +0530

    error file and structure setup

--- staking/programs/staking/src/instructions/mod.rs
+++ staking/programs/staking/src/instructions/mod.rs
@@ -1,3 +1,11 @@
-pub mod initialize;
+pub mod claim_rewards;
+pub mod initialise_config;
+pub mod initialise_user;
+pub mod stake;
+pub mod unstake;
 
-pub use initialize::*;
+pub use claim_rewards::*;
+pub use initialise_config::*;
+pub use initialise_user::*;
+pub use stake::*;
+pub use unstake::*;
