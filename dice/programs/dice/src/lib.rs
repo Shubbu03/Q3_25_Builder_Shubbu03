26abb52a32046079742a30693ce4abe5491feeb7
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-07-28 13:20:13 +0530

    dice program

--- dice/programs/dice/src/lib.rs
+++ dice/programs/dice/src/lib.rs
@@ -1,11 +1,10 @@
-pub mod constants;
+#![allow(unexpected_cfgs, deprecated)]
 pub mod error;
 pub mod instructions;
 pub mod state;
 
 use anchor_lang::prelude::*;
 
-pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
@@ -15,7 +14,21 @@ declare_id!("htoXsDnahPsRfEWD1ByD7oGiGyic5sL95N6sfVcTf6G");
 pub mod dice {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        ctx.accounts.init(amount)
+    }
+
+    pub fn place_bet(ctx: Context<PlaceBet>, seed: u128, roll: u8, amount: u64) -> Result<()> {
+        ctx.accounts.create_bet(&ctx.bumps, seed, roll, amount)?;
+        ctx.accounts.deposit(amount)
+    }
+
+    pub fn resolve_bet(ctx: Context<ResultBet>, sig: Vec<u8>) -> Result<()> {
+        ctx.accounts.verify_ed25519_signature(&sig)?;
+        ctx.accounts.resolve_bet(&ctx.bumps, &sig)
+    }
+
+    pub fn refund_bet(ctx: Context<RefundBet>) -> Result<()> {
+        ctx.accounts.refund_bet(&ctx.bumps)
     }
 }
